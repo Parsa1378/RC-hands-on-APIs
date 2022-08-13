0679f6250ed3f0c28c31f78384cc467bd656054f
Parsa Poorsistani <poorsistani13@gmail.com>
2022-08-13 14:09:57 +0430

    dotenv added and logics for token generating added

--- controller/logics.js
+++ controller/logics.js
@@ -1,15 +1,26 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 const login = (req, res) => {
     //todo:
     //input:phone output:success
-    res.status(200).json(req.body);
+    const phoneNumber = req.body;
+    if (phoneNumber) {
+        res.status(200).json(phoneNumber);
+    }
+    res.status(400).json({ msg: 'phone number missing' });
 };
 
 const confirm = (req, res) => {
     //todo:
     //output:toke
-    const code = 'sjdfbsdfbasbfidoabsfgbaldsbgls';
-    res.status(200).json({ msg: 'right' });
+    const code = '767654';
+    res.status(200).json({ token: generateAccessToken(code) });
 };
+
+const generateAccessToken = (code) => {
+    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '100000s' });
+}
 
 
 module.exports = {
